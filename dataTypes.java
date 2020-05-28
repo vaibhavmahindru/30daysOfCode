62fe7710cc197cea2ded72c613326de88176f158
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-05-28 06:13:33 +0530

    DAY 1

--- dataTypes.java
+++ dataTypes.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class dataTypes {
 	
     public static void main(String[] args) {
         int i = 4;
