81084ad15b110e1131c8dcfc5a67e9e64530a441
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-06-02 09:44:40 +0530

    Day 3

--- dataTypes.java
+++ dataTypes.java
@@ -1,34 +1,34 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class dataTypes {
-	
+
     public static void main(String[] args) {
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
-		
+
         Scanner scan = new Scanner(System.in);
 
         /* Declare second integer, double, and String variables. */
         int j;
         double d2;
         String s2;
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        /* Read and save an integer, double, and String to your variables. */
+        // Note: If you have trouble reading the entire String, please go back and
+        // review the Tutorial closely.
         j = scan.nextInt();
         d2 = scan.nextDouble();
+        scan.nextLine();// to skip the new lineor enter
         s2 = scan.nextLine();
         /* Print the sum of both integer variables on a new line. */
-        System.out.println(i+j);
+        System.out.println(i + j);
         /* Print the sum of the double variables on a new line. */
-		System.out.println(d+d2);
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-        System.out.println(s+s2);
+        System.out.println(d + d2);
+        /*
+         * Concatenate and print the String variables on a new line; the 's' variable
+         * above should be printed first.
+         */
+        System.out.println(s + s2);
         scan.close();
     }
 }
--- operators.java
+++ operators.java
@@ -1,20 +1,14 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class operators {
 
     // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        double tip = meal_cost*((double)tip_percent/100);
-        double tax = meal_cost*((double)tax_percent/100);
+        double tip = meal_cost * ((double) tip_percent / 100);
+        double tax = meal_cost * ((double) tax_percent / 100);
 
         double totalCost = meal_cost + tip + tax;
-        System.out.println(Math.round(totalCost));//rounds upto the neares integer.
+        System.out.println(Math.round(totalCost));// rounds upto the neares integer.
 
     }
 
